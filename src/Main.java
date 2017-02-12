a2ce785a739e25fdf54bab775b13436d645eeecd
jackfrancisdalton <jackfrancisdalton@gmail.com>
2017-02-12 18:21:47 +0100

    updated misc and added filereader example

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 import DateTime.DateTimeExamples;
 import Interface.FunctionalInterfaceExamples;
 import JavaFX.JavaFXExamples;
+import Misc.MiscExamples;
 import Optional.OptionalExamples;
 import Parallel.ParallelismExamples;
 import Stream.StreamExamples;
@@ -9,9 +10,10 @@ import Stream.StreamExamples;
 
 public class Main {
     public static void main(String[] args) {
-        JavaFXExamples.run(args);
+//        JavaFXExamples.run(args);
 //        StreamExamples.run();
 //        ParallelismExamples.run();
+//        MiscExamples.run();
 //        OptionalExamples.run();
 //        DateTimeExamples.run();
 //        new FunctionalInterfaceExamples().print("Hello");
